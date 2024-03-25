c86da3c567a468af0df943a97e088cc05a03a887
Akash <akash@pop-os.localdomain>
2024-03-25 17:44:38 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import bhashini from 'bhashini-translation';
+
+import asrRoutes from './routes/asr.routes.js';
+import asrNmtRoutes from './routes/asr_nmt.routes.js';
+import nmtRoutes from './routes/nmt.routes.js';
+import ttsRoutes from './routes/tts.routes.js';
+import nmtTtsRoutes from './routes/nmt_tts.routes.js';
+import asrNmtTtsRoutes from './routes/asr_nmt_tts.routes.js';
+
+import logger from './utils/logger.js';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Authentication
+const BHASHINI_USER_ID = 'user_id_value';
+const BHASHINI_ULCA_API_KEY = 'Ulca_api_value';
+const BHASHINI_INFERENCE_API_KEY = 'inference_api_value';
+bhashini.auth(BHASHINI_USER_ID, BHASHINI_ULCA_API_KEY, BHASHINI_INFERENCE_API_KEY);
+
+// Routes
+app.use('/asr', asrRoutes);
+app.use('/asr_nmt', asrNmtRoutes);
+app.use('/nmt', nmtRoutes);
+app.use('/tts', ttsRoutes);
+app.use('/nmt_tts', nmtTtsRoutes);
+app.use('/asr_nmt_tts', asrNmtTtsRoutes);
+
+// Default route
+app.get('/', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  logger.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong' });
+});
+
+export default app;
