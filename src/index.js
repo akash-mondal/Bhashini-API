b72e8fce61dc5e45f9a89f222fdd6fdbe273a726
Akash Mondal <96812236+akash-mondal@users.noreply.github.com>
2024-03-25 18:39:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import app from './app.js';
 import http from "http";
+import os from 'os';
+const host = os.hostname();
 const server = http.createServer(app);
 const PORT = 3003;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(`Access the server at http://${host}:${PORT}`);
 });
