970e353a6b47476c3078a12f59151bd2461ef6c3
Akash Mondal <96812236+akash-mondal@users.noreply.github.com>
2024-03-25 18:28:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import app from './app.js';
 import http from "http";
 import * as logger from "./utils/logger.js";
 const server = http.createServer(app);
-const PORT = process.env.PORT || 3003;
+const PORT = 3003;
 server.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
 });
